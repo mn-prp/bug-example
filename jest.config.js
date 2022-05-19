dc75462a41a1e9995cf2ab4dd6e242820d5670d1
Matthew Nelson <mnelson@precisionplanting.com>
2022-05-19 14:09:32 -0500

    Doesn't work

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,25 @@
 module.exports = {
-    preset: "ts-jest",
     testEnvironment: "jsdom",
     transformIgnorePatterns: ["node_modules/(?!ol|d3-array)/"],
     transform: {
-        "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.(t|j)sx?$": [
+            "@swc/jest",
+            {
+                sourceMaps: true,
+
+                jsc: {
+                    parser: {
+                        syntax: "typescript",
+                        tsx: true,
+                    },
+
+                    transform: {
+                        react: {
+                            runtime: "automatic",
+                        },
+                    },
+                },
+            },
+        ],
     },
-    // transform: {
-    //   "^.+\\.(t|j)sx?$": [
-    //     "@swc/jest",
-    //     {
-    //       sourceMaps: true,
-
-    //       jsc: {
-    //         parser: {
-    //           syntax: "typescript",
-    //           tsx: true,
-    //         },
-
-    //         transform: {
-    //           react: {
-    //             runtime: "automatic",
-    //           },
-    //         },
-    //       },
-    //     },
-    //   ],
-    // },
 };
